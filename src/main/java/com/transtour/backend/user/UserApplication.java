8efd6ecd39a7238e19557f0feed45b5235b6d492
charlyla <cnlaffitte@gmail.com>
2021-11-07 20:43:25 -0300

    subo enciptacion

--- src/main/java/com/transtour/backend/user/UserApplication.java
+++ src/main/java/com/transtour/backend/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.transtour.backend.user;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaRepositories("com.transtour.backend.user.repository")
+@EnableEncryptableProperties
 public class UserApplication {
 
 	public static void main(String[] args) {
