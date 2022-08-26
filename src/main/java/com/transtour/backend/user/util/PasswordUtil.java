35ea6c5cc486d98b979701436e37d6feaff748f5
pabloOcanto <pomalianni@gmail.com>
2022-08-25 23:40:01 -0300

    fix: Passwword generator con numeros!

--- src/main/java/com/transtour/backend/user/util/PasswordUtil.java
+++ src/main/java/com/transtour/backend/user/util/PasswordUtil.java
@@ -5,16 +5,16 @@ import java.util.Random;
 
 public class PasswordUtil {
 
-    private static final String VALID_PW_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+{}[]|:;<>?,./";
-    private static final int DEFAULT_PASSWORD_LENGTH = 6;
+    private static final String VALID_PW_CHARS = "0123456789";
+    private static final int DEFAULT_PASSWORD_LENGTH = 4;
     private static final Random RANDOM = new SecureRandom();
 
 
-    public static String radomGenerator(){
+    public static String radomGenerator() {
         String pw = "";
-        for (int i=0; i<DEFAULT_PASSWORD_LENGTH; i++) {
-            int index = (int)(RANDOM.nextDouble()*VALID_PW_CHARS.length());
-            pw += VALID_PW_CHARS.substring(index, index+1);
+        for (int i = 0; i < DEFAULT_PASSWORD_LENGTH; i++) {
+            int index = (int) (RANDOM.nextDouble() * VALID_PW_CHARS.length());
+            pw += VALID_PW_CHARS.substring(index, index + 1);
         }
         return pw;
     }
